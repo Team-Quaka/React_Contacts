0643918f0592c80aecd1b833cc245b8fa106ebbd
saniyar-dev <saniyar.dev@gmail.com>
2022-08-16 16:29:27 +0430

    signup page init
    good stage
    
    signup page khoshgelasion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@mui/material'
 import { useRecoilState } from 'recoil'
 import { AuthState } from '../state/AuthState'
 
 const Home: NextPage = () => {
   const [Auth, setAuth] = useRecoilState(AuthState)
   return (
-    <div>start template</div>
+    <div className='flex flex-col justify-center items-center h-screen'>{
+      Auth.logedIn === false ? 
+      <Link href={'/signup'}>
+        <Button variant='outlined'>start</Button>
+      </Link> : 
+      <Link href={'/contacts'}>
+        <Button variant='outlined'>start</Button>
+      </Link>
+    }</div>
   )
 }
 
---
+++ pages/signup.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from "next";
+import {Input, Button} from '@mui/material'
+import { useCallback, useState } from "react";
+
+const Signup: NextPage = () => {
+    const [inputValue, setInputValue] = useState('')
+
+    const signupHandler = useCallback(() => {
+        console.log('handle signup')
+        console.log(inputValue)
+    }, [inputValue])
+    return (
+        <div className="flex justify-center flex-col h-screen w-1/3 mx-auto ">
+            <h1 className="text-3xl mb-9 font-bold">
+                Please Signup to continue
+            </h1>
+            <Input type="text" onChange={(e) => {
+                e.preventDefault()
+                setInputValue(e.target.value)
+                console.log(109)
+            }} value={inputValue} />
+            <div className="mt-6">
+                <Button variant="outlined" onClick={(e) => {
+                    e.preventDefault()
+                    signupHandler()
+                }}>
+                    signup
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Signup
